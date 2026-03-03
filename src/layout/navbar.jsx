b77095bc5735c058b1ba1a5f728e3db4d099db1a
mshubham17 <mehtashubham172@gmail.com>
2026-03-03 16:42:54 +0530

    About section projects section design

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,6 +1,6 @@
 import { Button } from "../components/button";
 import { Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const navLinks = [
   { href: "#About", label: "About" },
@@ -10,8 +10,24 @@ const navLinks = [
 
 export const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
+    <header
+      className={`fixed top-0 left-0 right-0 transition-all duration-500 ${
+        isScrolled ? "glass-strong py-3" : "bg-transparent py-5"
+      }  z-50`}
+    >
       <nav className=" container mx-auto px-6 flex items-center justify-between">
         <a
           href="#"
@@ -20,7 +36,7 @@ export const Navbar = () => {
           SM <span className="text-primary">.</span>
         </a>
         {/* Desktop */}
-        <div className="hidden md:flex items-center gap-6">
+        <div className="hidden md:flex items-center gap-1">
           <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
             {navLinks.map((link, index) => (
               <a
@@ -28,7 +44,6 @@ export const Navbar = () => {
                 key={index}
                 className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface"
               >
-                {" "}
                 {link.label}
               </a>
             ))}
@@ -56,9 +71,9 @@ export const Navbar = () => {
               <a
                 href={link.href}
                 key={index}
+                onClick={() => setIsMobileMenuOpen(false)}
                 className="text-lg text-muted-foreground hover:text-foreground py-2"
               >
-                {" "}
                 {link.label}
               </a>
             ))}
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,129 @@
+import {
+  Code2,
+  Headphones,
+  Lightbulb,
+  MoveIcon,
+  Rocket,
+  Theater,
+  TheaterIcon,
+  Tv,
+  Users,
+} from "lucide-react";
+import { useState } from "react";
+const highlights = [
+  {
+    icon: Headphones,
+    title: "Last played",
+    description: "Blinding Lights — The Weeknd",
+  },
+  {
+    icon: Tv,
+    title: "Last watched",
+    description: "One Battle After Another",
+  },
+];
+const quotes = [
+  {
+    text: "Hard work beats talent when talent doesn't work hard.",
+    author: "Tim Notke",
+  },
+  {
+    text: "Success is nothing more than a few simple disciplines, practiced every day.",
+    author: "Jim Rohn",
+  },
+  {
+    text: "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    author: "Commonly attributed to Aristotle",
+  },
+  {
+    text: "The man who moves a mountain begins by carrying away small stones.",
+    author: "Confucius",
+  },
+  {
+    text: "He who has a why to live can bear almost any how.",
+    author: "Friedrich Nietzsche",
+  },
+  {
+    text: "It always seems impossible until it's done.",
+    author: "Nelson Mandela",
+  },
+  {
+    text: "Integrity is doing the right thing, even when no one is watching.",
+    author: "C. S. Lewis",
+  },
+  {
+    text: "The price of greatness is responsibility.",
+    author: "Winston Churchill",
+  },
+];
+
 export const About = () => {
-  return <section></section>;
+  const [quote] = useState(quotes[Math.floor(Math.random() * quotes.length)]);
+  return (
+    <section id="about" className="py-32 relative overflow-hidden">
+      <div className="container mx-auto px-6 relative z-10">
+        <div className="grid lg:grid-cols-2 gap-16 items-center">
+          {/* Left Column */}
+          <div className="space-y-8">
+            <div className="animate-fade-in">
+              <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase">
+                About Me
+              </span>
+            </div>
+
+            <h2 className="text-4xl md:text-5xl font-bold leading-tight animate-fade-in animation-delay-100 text-secondary-foreground">
+              Figuring it out,
+              <span className="font-serif italic font-normal text-white">
+                {" "}
+                one chapter at a time.
+              </span>
+            </h2>
+
+            <div className="space-y-4 text-muted-foreground animate-fade-in animation-delay-200">
+              <p>
+                I'm an aspiring Full-Stack Developer and a passionate learner
+                who believes that how we build is just as important as what we
+                build.
+              </p>
+              <p>
+                When I'm not coding, you'll find me binge watching movies and tv
+                shows. I enjoy watching films, reading, and exploring topics
+                that expand my perspective from history and geopolitics to
+                aviation and architecture. I'm naturally curious about how
+                systems work, whether in technology or the real world.
+              </p>
+            </div>
+
+            <div className="glass rounded-2xl p-6 glow-border animate-fade-in animation-delay-300">
+              <p className="text-lg font-medium  text-foreground">
+                “{quote.text}”
+              </p>
+              <p className="mt-4 text-sm font-medium italic text-muted-foreground">
+                — {quote.author}
+              </p>
+            </div>
+          </div>
+
+          {/* Right Column - Hilights */}
+          <div className="grid sm:grid-cols-2 gap-6">
+            {highlights.map((item, idx) => (
+              <div
+                key={idx}
+                className="glass p-6 rounded-2xl animate-fade-in"
+                style={{ animationDelay: `${(idx + 1) * 100}ms` }}
+              >
+                <div className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center mb-4 hover:bg-primary/20">
+                  <item.icon className="w-6 h-6 text-primary" />
+                </div>
+                <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
+                <p className="text-sm text-muted-foreground">
+                  {item.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
